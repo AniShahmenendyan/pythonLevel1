021adfed047cfd377e01d85c3869ca55dd709778
Felix Kocharyan <felix@embry.tech>
2022-02-23 21:11:56 +0400

    dunder methods exercises

---
+++ OOP dunder methods exercises partially completed.py
@@ -0,0 +1,163 @@
+# 1. Create a custom integer class. The class should have one instance attribute as its value. Now, your class must
+# satisfy the following criteria:
+#    a) we can use Python arithmetic operators to add, subtract, multiply and divide the values.
+#    b) create another class called Inf. For the sake of consistency, this class may inherit from int class.
+#    c) if we divide by zero, we don't get an exception. Instead, we get inf, which was implemented in our previous step
+#    d) when we print our class, we want it to be represented as nicely as possible. As our class represents an integer
+#    number, we want to see the value when we print it or cast it to a string.
+#    e) as with every number, we want to be able to compare our integer instances using the logical operators.
+#    Implement those as well.
+#    f) finally, there is one thing that all Python objects have in common. If we neglect 0, '', None, False etc., then
+#    every object holds a value of True when casted to bool. We want our Integer to behave as a normal integer in this
+#    sense. When we cast an Integer to bool, we must get False, if the value of our object is 0. True otherwise.
+
+
+class Integer(int):
+    def __init__(self, val):
+        if type(val) != int:
+            raise TypeError('Integer value must be int')
+        self.val = val
+
+    def __add__(self, other):
+        self.type_check(other)
+        return Integer(self.val + other.val)
+
+    def __sub__(self, other):
+        self.type_check(other)
+        return Integer(self.val - other.val)
+
+    def __mul__(self, other):
+        self.type_check(other)
+        return Integer(self.val * other.val)
+
+    def __truediv__(self, other):
+        self.type_check(other)
+        if other.val == 0:
+            return Inf()
+
+        return self.val / other.val
+
+    def __bool__(self):
+        return False if self.val == 0 else True
+
+    @staticmethod
+    def type_check(second_operand):
+        if not isinstance(second_operand, Integer):
+            raise TypeError('instances must be Integer')
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Inf(Integer):
+    def __init__(self, val=0):
+        super().__init__(val)
+
+    def __str__(self):
+        return 'inf'
+
+
+# x = Integer(True)
+# y = Integer(0)
+#
+# print(x)
+
+
+# print(x / y)
+#
+# print(x + z)
+# 2. Create a Color class. This will hold 3 instance variables, red, green, blue (RGB).
+# Each parameter (red, green, and blue) defines the intensity of the color as an integer between 0 and 255. We want to be able to get new
+# colors if we add instances of the color class. I'm not sure how much sense it will make to also subtract the colors,
+# but lets do it for fun! In summary, we can add 2 or more colors and get a new color object with added RGB values
+# (don't forget about the boundaries!), subtract them to get a new color object with subtracted values. When printed,
+# we want to have a nice representation of our color (maybe even the color itself?). One more fun thing! As colors
+# are quite often represented in hexadecimals, lets override the corresponding function such that when hex() is called
+# on our class, we get the hex color code for our color.
+
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    # def r_getter(self):
+    #     return self._r
+    #
+    # def r_setter(self, r):
+    #     if r in range(0, 256):
+    #         self._r = r
+    #     else:
+    #         raise ValueError
+    #
+    # r = property(r_getter, r_setter)
+
+    # @property
+    # def rgb(self):
+    #     return self.__red, self.__green, self.__blue
+    #
+    # @rgb.setter
+    # def rgb(self, rgb):
+    #     r, g, b = rgb
+    #
+    #     self.__red = self.color_range(r)
+    #     self.__green = self.color_range(g)
+    #     self.__blue = self.color_range(b)
+
+    @property
+    def r(self):
+        return self.__red
+
+    @r.setter
+    def r(self, r):
+        self.__red = self.color_range(r)
+
+    @property
+    def g(self):
+        return self.__green
+
+    @g.setter
+    def g(self, g):
+        self.__green = self.color_range(g)
+
+    @property
+    def b(self):
+        return self.__blue
+
+    @b.setter
+    def b(self, b):
+        self.__blue = self.color_range(b)
+
+    def __add__(self, other):
+        self.type_check(other)
+        return Color(self.r + other.r, self.g + other.g, self.b + other.b)
+
+    def __sub__(self, other):
+        self.type_check(other)
+        return Color(self.r - other.r, self.g - other.g, self.b - other.b)
+
+    @staticmethod
+    def type_check(second_operand):
+        if not isinstance(second_operand, Color):
+            raise TypeError('instances must be Color')
+
+    @staticmethod
+    def color_range(v):
+        return max(0, min(v, 255))
+
+    def rgb_hex(self):
+        return '#{:02x}{:02x}{:02x}'.format(self.r, self.g, self.b)
+
+    # def __hex__(self):
+
+    def __str__(self):
+        return f'r:{self.r}, g:{self.g}, b:{self.b}'
+
+
+c = Color(10, 20, 30)
+c_2 = Color(20, 30, 250)
+print(c + c_2)
+# c.r_setter(300)
+# c.r = 300
+# c.g = -200
+# print(c)
---
+++ OOP homework (properties, dunder methods.py
@@ -0,0 +1,76 @@
+# 1. Create an Atom class. We will have predefined atoms for it, which are C - carbon, N - nitrogen, H - hydrogen,
+# O - oxygen and P - phosphorus.
+#     a) Now, the class has an instance variable called "name".
+#     b) If you create an object with a name other than the 5 atoms mentioned, you should get an exception. But not any
+#     kind of exception. This exception will be something that we have defined. So, define an exception class called
+#     UnknownAtom. This must give us a detailed explanation why it is raised.
+#     c) Define a new class called Molecule. The molecule has an instance variable, which is a list.
+#     d) implement the __add__ method for our Atom class. Adding two atoms will return, you've guessed it, a Molecule!
+#     e) We can also add an atom to a molecule. So implement that functionality as well!
+#     f) Finally, do some cosmetic stuff, like implement the __str__ method for both of our classes.
+# If you're into chemistry, feel free to add some logic like keeping charges of each atom, tracking valency and raising
+# an error if two atoms can't make a molecule.
+
+# Ստեղծել Atom կլաս։ Կլասի մեջ պետք է ունենանք 5 սահմանված ատոմ՝ C ածխածին, N ազոտ, H ջրածին, O թթվածին և P ֆոսֆոր։
+#     a) Կլասը պետք է ունենա "name" կոչվող instance attribute
+#     b) Եթե փորձենք ատոմ ստեղծել, որը չկա մեր ցուցակի մեջ, պետք է ստանանք exception։ Ստեղծել UnknownAtom կոչվող
+#     exception, որը կստանանք, եթե վերը նշված դեպքը լինի։
+#     c) Ստեղծել Molecule կլաս։ Մոլեկուլն ունի մեկ instance attribute, որը list է։
+#     d) Սահմանելով գումարման օպերատորը, այնպես անել, որ երկու ատոմ իրար գումարելիս ստանանք մոլեկուլ:
+#     e) Մոլեկուլին նույնպես պետք է կարողանանք ատոմ գումարել։ Դա նույնպես սահմանել։
+#     f) Վերջապես, սահմանել __str__, __repr__ այս ֆունկցիաներից մեկը մեր բոլոր կլասերի համար, որպեսզի որպես սթրինգ
+#     ներկայացումը գեղեցկանա
+# Լրացուցիչ, եթե քիմիայի սիրահար եք, կարող եք ատոմներին ավելացնել լիցքեր, սահմանել վալենտականությունը և դրանց հիման վրա
+# փոփոխել գումարման տրամաբանությունը։
+
+# 2. Let's write a Music Player Class!
+#    a) Create a Song class. The class will have 4 attributes - name, artist, album and the year.
+#    b) Now let's create a Playlist. Playlist class will contain Songs. We should have a method that will load songs
+#    into our playlist. A file called albums.txt is provided with this exercise. The method should take care of loading
+#    the songs from the file and store them inside of our Playlist class.
+#    c) Now, we need our Player itself. Create a Player class. The player may contain at least one playlist. A few
+#    of its methods include play(), that will start playing from the beginning, show_now_playing, that will show the
+#    information of the song that is playing now, next_song, that will start playing the next_song, prev_song, that does
+#    the opposite and finally a stop() method, that stops the song that is playing.
+#    d) Finally, implement __str__ method for our classes, so we can see a nice representation of each object.
+# Note: The aforementioned points are necessary but it's not a complete description of a music player. Be creative
+# and add more functionality wherever you'll find it useful!
+
+# Եկեք ստեղծենք նվագարկիչի կլաս։ Կառուցվածքը հետևյալն է լինելու։ Ունենալու ենք երեք կլաս՝ Player, Playlist, Song:
+# Song-ը պարունակելու է երգի մասին ինֆորմացիա, Playlist-ը պարունակելու է երգերը Song տիպի օբյեկտների տեսքով, իսկ
+# Player-ը ունենալու է Playlist:
+#    a) Ստեղծել Song կլաս։ Կլասը պետք է ունենա 4 ատրիբուտ - name, artist, album, year
+#    b) Ստեղծենք Playlist կլասը։ Այն պարունակելու է երգերը։ Այս կլասը պետք է ունենա load songs մեթոդ։ Տվյալ տնայինի հետ
+#    ձեզ եմ ուղարկում նաև albums.txt ֆայլը։ Ֆայլը պարունակում է հարյուրավոր երգերի անունները, հեղինակներին, ալբոմների
+#    անունները և ձայնագրման թվականը։ Ամեն տողում մի երգի ինֆորմացիա է։ Ամեն տողի դաշտերը իրարից անջատված են tab-երով։
+#    Վերոնշյալ մեթոդով պետք է կարդալ ֆայլը և բոլոր երգերը Song-ի տեսքով փոխանցենք Playlist-ին։
+#    c) Ստեղծել Player կլասը։ Այն պետք է ունենա Playlist տիպի ատրիբուտ։ Այս կլասը պետք է ունենա նաև հետևյալ մեթոդները՝
+#    play(), stop(), next_song(), previous_song(), show_current_song()։ Մեթոդները կանչելուց պետք է կոնսոլում տեսնենք,
+#    թե որ երգն է տվյալ պահին միացած։ Նաև վալիդացիաներ են պետք։ Օրինակ, եթե նվագարկիչն անջատված է, հաջորդ երգին անցնել
+#    կամ կրկին անջատել չենք կարող։
+#    d) Որպեսզի ամեն երգերը ավելի գեղեցիկ ներկայացվեն, սահմանել __str__ մեթոդը։ Ցանկալի է բոլոր կլասերի համար։
+
+# Այս կետերը պարտադիր են, սակայն չեն պարունակում նվագարկիչի ողջ բնութագրությունը։ Կարող եք ազատ ավելացնել հավելյալ
+# տրամաբանություն։
+
+# 3. Create a class named Length. The default unit for length is meter. The class must contain some conversions
+# information, e.g. feet -> m, km -> m, yard -> m, mile -> m etc.
+#    a) Create a dictionary that will hold the metrics. Keys will be the unit name and their values will be the
+#    coefficients for converting that unit to meters.
+#    b) The class will have 2 instance attributes. Units and the length value itself.
+#    c) Now, we can add lengths of course. So implement that method. But be careful, we can't add yard to meters, so
+#    you will need to convert everything before adding.
+#    d) Implement the __str__ method. This method must show the length of our Length object in meters.
+#    e) Implement the __repr__ method. This method must show the length in whichever units our class is.
+# Feel free to add some more features if you find them useful.
+
+# Ստեղծել Length կլաս։ Երկարության հիմնական միավորը կհամարենք մետրը։ Կլասը պետք է ունենա որոշակի ինֆորմացիա, թե ինչպես
+# փոխակերպել այլ միավորը մետրի (feet, km, yard, mile etc.)
+#    a) Ստեղծել բառարան, որը կպահի վերոնշյալ ինֆորմացիան։ Բանալիները կլինեն միավորների անունները, իսկ արժեքները կլինեն
+#    այն գործակիցները, որոնց օգնությամբ այդ միավորից ստանում ենք մետր։
+#    b) Կլասը պետք է ունենա երկու instance attributes։ Երկարության արժեքը և միավորը։
+#    c) Մենք պետք է կարողանանք երկարությունները իրար գումարել։ Սահմանել համապատասխան մեթոդը։ Ուշադրություն դարձնել
+#    միավորներին։ Մենք չենք կարող ուղղակիորեն մետրը գումարել մղոնի կամ հակառակը։
+#    d) Սահմանել __str__ մեթոդը։ Այս մեթոդը մեզ ցույց կտա օբյեկտի երկարությունը մետրերով։
+#    e) Սահմանել __repr__ մեթոդը։ Այս մեթոդը մեզ ցույց կտա օբյեկտի երկարությունը այն միավորով, որով սահմանվել է։
+# Լրացուցիչ, կարող եք ավելացնել հավելյալ տրամաբանություն որտեղ ճիշտ կգտնեք։
