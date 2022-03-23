23c87cf44066db758cb22784a8610b6a93459962
Felix Kocharyan <felix@embry.tech>
2022-03-23 23:37:24 +0400

    add simple server and flask server

--- web/server_sample.py
+++ web/server_sample.py
@@ -2,15 +2,28 @@ from wsgiref.simple_server import make_server
 
 
 def app(environ, start_response):
-    response_body = b'Hello World!!!'
+    response_body = ''
+
+    request_path = environ['PATH_INFO']
+    request_query_string = environ['QUERY_STRING']
+    request_method = environ['REQUEST_METHOD']
+
+    users = ['Felix', 'Mher', 'Arsen', 'Davit']
     response_status = '200 OK'
 
+    if request_path == '/':
+        response_body = 'Home page!!!'
+    elif request_path == '/users':
+        response_body = str(users)
+    else:
+        response_body = 'Not found'
+        response_status = '404 NOT FOUND'
+
     response_headers = [
-        ('Location', 'https://google.com'),
         ('content-type', 'text/plain'),
-        ('content-length', str(len(response_body)))
     ]
-    response_status = '302 '
+
+    response_body = response_body.encode()
 
     start_response(response_status, response_headers)
 
@@ -18,7 +31,7 @@ def app(environ, start_response):
 
 
 srv = make_server(
-    '0.0.0.0',
+    'localhost',
     8000,
     app
 )
